7260e08711602d48b9801b5fa5e9aeb677419c84
ikrom97 <fleck97rgb@gmail.com>
2021-10-08 16:19:17 +0500

    services and workSystem pages made up

--- resources/views/pages/medservices/index.blade.php
+++ resources/views/pages/medservices/index.blade.php
@@ -1,7 +1,204 @@
 @extends('layouts.app')
 
 @section('content')
-   <main class="medservices">
-      <h1>Medservices</h1>
+   <main class="medservice-page">
+      <setion class="medservice">
+         <div class="container">
+            <h1 class="heading txt-center">{{__('messages.medservices')}}</h1>
+            <p class="medservice__text txt-center">{{__('messages.medservicesText')}}</p>
+            <ul class="services">
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service1.png')}}">
+                  <h3 class="services__title">Стоматология</h3>
+                  <ul>
+                     <li>Протезирование зубов</li>
+                     <li>Дентальная имплантация</li>
+                     <li>Детская стоматология</li>
+                     <li>Протезирование зубов</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service2.png')}}">
+                  <h3 class="services__title">Диагностика & CHECK-UP</h3>
+                  <ul>
+                     <li>Все виды диагностических услуг</li>
+                     <li>Радионуклидная диагностика</li>
+                     <li>МРТ, КТ, УЗИ, ПЭТ, коронарография</li>
+                     <li>Диагностика CHECK-UP</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service3.png')}}">
+                  <h3 class="services__title">Пластическая хирургия</h3>
+                  <ul>
+                     <li>Реконструктивная хирургия</li>
+                     <li>Косметическая хирургия</li>
+                     <li>Эндоскопические технологии</li>
+                     <li>Процедуры интимной хирургии</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service4.png')}}">
+                  <h3 class="services__title">Акушерство и гинекология</h3>
+                  <ul>
+                     <li>Лечение женского бесплодия</li>
+                     <li>Лечение эрозии шейки матки</li>
+                     <li>Лечение заболеваний яичников</li>
+                     <li>ЭКО</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service5.png')}}">
+                  <h3 class="services__title">Офтальмология</h3>
+                  <ul>
+                     <li>Лазерная коррекция зрения</li>
+                     <li>Кросслинкинг роговицы</li>
+                     <li>Кератопластика</li>
+                     <li>Лечение глаукомы и катаракты</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service6.png')}}">
+                  <h3 class="services__title">Лечение онкологии</h3>
+                  <ul>
+                     <li>Брахитерапия</li>
+                     <li>Лучевая терапия</li>
+                     <li>«Терапия цели»</li>
+                     <li>Химиотерапия</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service7.png')}}">
+                  <h3 class="services__title">Кардиология и кардиохирургия</h3>
+                  <ul>
+                     <li>Радиочастотная абляция (РЧА)</li>
+                     <li>Консультации, диагностика и лечение</li>
+                     <li>Ангиопластика</li>
+                     <li>Установка кардиостимулятора сердца</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service8.png')}}">
+                  <h3 class="services__title">Урология и нефрология</h3>
+                  <ul>
+                     <li>Болезнь Пейрони</li>
+                     <li>Гемодиализ</li>
+                     <li>Лазерное лечение недержания мочи</li>
+                     <li>Урофлоуметрия</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service9.png')}}">
+                  <h3 class="services__title">Хирургия</h3>
+                  <ul>
+                     <li>Холецистэктомия</li>
+                     <li>Удаление гемангиомы</li>
+                     <li>Лечение трофических язв</li>
+                     <li>Видеоторакоскопия</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service10.png')}}">
+                  <h3 class="services__title">Неврология и нейрохирургия</h3>
+                  <ul>
+                     <li>Лечение болезни Паркинсона</li>
+                     <li>Иглорефлексотерапия</li>
+                     <li>Рассеянный склероз</li>
+                     <li>Вегето-сосудистая дистония</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service11.png')}}">
+                  <h3 class="services__title">Ортопедия и травматология</h3>
+                  <ul>
+                     <li>Лечение артроза</li>
+                     <li>Операции после спортивных травм</li>
+                     <li>Вертебропластика</li>
+                     <li>Эндопротезирование крупных суставов</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+               <li class="services-item">
+                  <img class="services__img" src="{{asset('img/service12.png')}}">
+                  <h3 class="services__title">Диетология</h3>
+                  <ul>
+                     <li>Индивидуализация рациона</li>
+                     <li>Снижение веса</li>
+                     <li>Здоровое питание</li>
+                     <li>Снижение веса</li>
+                  </ul>
+                  <a class="services__further" href="#">{{__('messages.further')}}</a>
+               </li>
+            </ul>
+         </div>
+      </setion>
+      <section class="desease">
+         <div class="container">
+            <h2 class="title txt-center">{{__('messages.deseasesAndTreatment')}}</h2>
+            <p class="deseases__text txt-center">{{__('messages.deseasesTreatmentText')}}</p>
+            <ul class="deseases-list">
+               <li class="deseases-item">
+                  <h3 class="deseases-title">Стоматология</h3>
+                  <ul>
+                     <li>Общая стоматология</li>
+                     <li>Имплантация зубов</li>
+                     <li>Виниры \ люминиры</li>
+                     <li>3D улыбка</li>
+                     <li>Коронки</li>
+                  </ul>
+               </li>
+               <li class="deseases-item">
+                  <h3 class="deseases-title">Эстетическая медицина</h3>
+                  <ul>
+                     <li>Пересадка волос</li>
+                     <li>Маммопластика пластика груди</li>
+                     <li>Программа похудения</li>
+                     <li>СПА-программы</li>
+                     <li>Косметология</li>
+                  </ul>
+               </li>
+               <li class="deseases-item">
+                  <h3 class="deseases-title">Офтальмология</h3>
+                  <ul>
+                     <li>Общие процедуры</li>
+                     <li>Лазерная коррекция зрения</li>
+                     <li>Глаукома</li>
+                     <li>Катаракта</li>
+                     <li>Трансплантация роговицы</li>
+                  </ul>
+               </li>
+               <li class="deseases-item">
+                  <h3 class="deseases-title">Онкология</h3>
+                  <ul>
+                     <li>Общее онкологическое лечение</li>
+                     <li>Лечение лейкемии</li>
+                     <li>Лечение рака желудка</li>
+                     <li>Лечение рака простаты</li>
+                     <li>Лечение рака горла</li>
+                  </ul>
+               </li>
+               <li class="deseases-item">
+                  <h3 class="deseases-title">Кардиология</h3>
+                  <ul>
+                     <li>Общее кардиологическое лечение</li>
+                     <li>Ишемическая болезнь сердца</li>
+                     <li>Ангиопластика</li>
+                     <li>Ишемическая болезнь сердца</li>
+                     <li>Ангиопластика</li>
+                  </ul>
+               </li>
+            </ul>
+         </div>
+      </section>
    </main>
 @endsection
--- resources/views/pages/worksystem/index.blade.php
+++ resources/views/pages/worksystem/index.blade.php
@@ -2,6 +2,62 @@
 
 @section('content')
    <main class="worksystem">
-      <h1>Worksystem</h1>
+      <div class="container">
+         <h1 class="heading txt-center">{{__('messages.workSystemHenesMed')}}</h1>
+         <p class="txt-center">{{__('messages.workSystemText')}}</p>
+         <section class="process">
+            <h2 class="title txt-center">{{__('messages.processTitle')}}:</h2>
+            <ol class="process-list">
+               <li class="process-item">
+                  <h3 class="process-item__title">Вы отправляете заявку</h3>
+                  <p class="process-item__text">
+                     Для этого воспользуйтесь нашей контактной формой, указав подробную информацию о заболевании и состоянии пациента и приложив соответствующие документы. Не забудьте проверить правильность вашего e-mail.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Отправляем вам анкету пациента</h3>
+                  <p class="process-item__text">
+                     Вам будет необходимо заполнить анкету пациента, чтобы наши специалисты могли подготовить для вас предложение по плану лечения.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Предложение плана лечения</h3>
+                  <p class="process-item__text">
+                     Наши специалисты отправят предложение плана лечения в течение 48 часов после того, как вы отправите анкету пациента.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Предложение плана путешествия</h3>
+                  <p class="process-item__text">
+                     После того, как вы подтвердите предложение плана лечения, наши специалисты подготовят для вас индивидуальный план путешествия с пакетными предложениями, в которые будут включены авиабилеты, трансфер из аэропорта и варианты размещения в отеле.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Подписание договора</h3>
+                  <p class="process-item__text">
+                     После того, как вы подтвердите план лечения и поездки, наши специалисты вышлют вам договор на подпись.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Оплата</h3>
+                  <p class="process-item__text">
+                     Вам будет необходимо произвести соответствующие оплаты. Все оплаты, связанные с лечением, будут производиться напрямую больницам и клиникам. Все платежи, связанные с поездкой, будут производиться напрямую нам - Tedavi Tour.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Начало лечения</h3>
+                  <p class="process-item__text">
+                     Мы будем сопровождать вас на протяжении всего лечения. Наши специалисты будут готовы удовлетворить все ваши дополнительные запросы.
+                  </p>
+               </li>
+               <li class="process-item">
+                  <h3 class="process-item__title">Завершение лечения</h3>
+                  <p class="process-item__text">
+                     Завершение лечения. Получение назначения врача по дальнейшему выздоровлению. Все документы, которые вам выпишет врач, мы отправим на ваш e-mail и напомним о повторном визите при необходимости.
+                  </p>
+               </li>
+            </ol>
+         </section>
+      </div>
    </main>
 @endsection
